bd4617dfd04596a18b7086d65395205524b1cabf
anzalmohamed <anzaldidah@gmail.com>
2021-04-10 03:57:21 +0300

    added files

---
+++ car-dealership/Animals.java
@@ -0,0 +1,12 @@
+//Create a class for animals available for adoption at a local shelter. Give the class relevant properties, such as age or species. Practice writing the class declaration and get familiar with how to use it. Also, make sure to save your work! The next few practice exercises in this section will build upon this exercise.
+
+//Create a command line interface for the animal shelter application you've built over the past several practice exercises. Create several instances of your Animal class in the main() method of your command line interface file. Then, add code to print all details about every animal back to the user.
+//Add at least one method to your Animal class from the last several exercises. It should return information based on several attributes of your Animal objects. For instance, an insidePet()method could return whether or not an adoptable animal would be recommended to live inside based on its species, size, or whatever other attributes your class contains. Then, add it to your command line application.
+
+// Create a constructor for the Animal class of the animal shelter application you've been working on throughout the previous several practice exercises. Refactor your command line interface to create Animals with your new constructor.
+
+// Add an attribute to your Animal class called mAdopted. This attribute should be a booleantype, and should indicate whether an animal has found a home yet, or is still in the shelter. Write a method that changes the mAdopted attribute of an animal from false to true.
+
+public class Animals {
+    public String mSpeices;
+}
